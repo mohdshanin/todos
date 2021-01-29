a5668b8c71e85d4f3ee00b270a5ae885436ae074
mohdshanin <shansid2000@gmail.com>
2021-01-29 23:13:14 +0530

    change

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { IncomeExpences } from "./Components/IncomeExpences";
 import { TransactionList } from "./Components/TransactionList";
 import { AddTransaction } from "./Components/AddTransaction";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Heading />
       <div className="container">
         <Balance />
@@ -17,7 +19,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,13 @@
+import { Transaction } from "../Components/Transaction";
+
+export default (state, action) => {
+    switch (action.type) {
+        case "DELETE_TRANSACTION":
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+       default:
+           return state;
+    }
+}
